c2994272702284ad20b189426dcd293c72c15b76
Ivan Senic <ivan.senic@novatec-gmbh.de>
2018-04-27 17:42:58 +0200

    closes #5: script for finding missing connections

--- src/merge-all.js
+++ src/merge-all.js
@@ -5,55 +5,76 @@ const glob = require('glob');
 const path = require('path');
 
 const components = 'components';
-var parentDir = path.join(__dirname, '../');
+const parentDir = path.join(__dirname, '../');
 
-// output file
-var all = {
-      components: []
+generateModel = function (doFlush) {
+      // output file
+      var all = {
+            components: []
+      }
+
+      // read all component files
+      var sources = [parentDir + '/model/components/**/*.yml'];
+      var files = [].concat.apply([], sources.map(g => glob.sync(g, { nodir: true })));
+
+      files.forEach(file => {
+            if (file.endsWith('.yml')) {
+                  console.info('Trying to merge file: ', file);
+                  var doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+
+                  if (doc !== undefined && doc[components] !== undefined) {
+                        all[components] = all[components].concat(doc[components]);
+                        console.info('Merged..');
+                  } else {
+                        console.warn('Skipping as no components defined..');
+                  }
+            }
+      });
+
+      // add all general files
+      var sources = [parentDir + '/model/*.yml'];
+      var files = [].concat.apply([], sources.map(g => glob.sync(g, { nodir: true })));
+
+      files.forEach(file => {
+            if (file.endsWith('.yml')) {
+                  console.info('Trying to merge file: ', file);
+                  var doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+
+                  if (doc !== undefined) {
+                        for (var property in doc) {
+                              all[property] = doc[property];
+                              console.info('Merged ', property, '..');
+                        }
+                  } else {
+                        console.warn('Skipping as no components defined..');
+                  }
+            }
+      });
+
+      if (doFlush) {
+             // dump combined objects as JSON
+            const result = JSON.stringify(all, null, 4);
+            // write to file
+            console.info('Writing to build/output/model.json');
+            mkdirp.sync(parentDir + '/build');
+            fs.writeFileSync(parentDir + '/build/output/model.json', result);
+      }
+
+      return all;
 }
 
-// read all component files
-var sources = [parentDir + '/model/components/**/*.yml'];
-var files = [].concat.apply([], sources.map(g => glob.sync(g, { nodir: true })));
+exports.model = function () {
+      // silent info while running
+      var ___info = console.info;
+      console.info = function(){};
 
-files.forEach(file => {
-      if (file.endsWith('.yml')) {
-            console.log('Trying to merge file: ', file);
-            var doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+      var model = generateModel(false);
 
-            if (doc !== undefined && doc[components] !== undefined) {
-                    all[components] = all[components].concat(doc[components]);
-                    console.info('Merged..');
-            } else {
-                    console.warn('Skipping as no components defined..');
-            }
-      }
-});
+      console.info = ___info;
+      return model; 
+}
 
-// add all general files
-var sources = [parentDir + '/model/*.yml'];
-var files = [].concat.apply([], sources.map(g => glob.sync(g, { nodir: true })));
+exports.modelAndFlush = function () {
+      return generateModel(true);
+}
 
-files.forEach(file => {
-      if (file.endsWith('.yml')) {
-            console.log('Trying to merge file: ', file);
-            var doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
-
-            if (doc !== undefined) {
-                   for (var property in doc) {
-                        all[property] = doc[property];
-                        console.info('Merged ', property, '..');
-                   }
-            } else {
-                  console.warn('Skipping as no components defined..');
-            }
-      }
-});
-
-// dump combined objects as JSON
-const result = JSON.stringify(all, null, 4);
-
-// write to file
-console.info('Writing to build/output/model.json');
-mkdirp.sync(parentDir + '/build');
-fs.writeFileSync(parentDir + '/build/output/model.json', result);
